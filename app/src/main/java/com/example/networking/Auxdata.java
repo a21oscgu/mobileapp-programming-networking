1654b20cc84a48078fdffcecd8e13547345b12e3
a21oscgu <a21oscgu@student.his.se>
2022-04-27 20:12:29 +0200

    Created Java classes

---
+++ app/src/main/java/com/example/networking/Auxdata.java
@@ -0,0 +1,16 @@
+package com.example.networking;
+
+/*
+
+"auxdata": {
+    "wiki": "https://en.wikipedia.org/wiki/K2",
+    "img": "https://en.wikipedia.org/wiki/K2#/media/File:K2_2006b.jpg"
+  }
+
+ */
+
+public class Auxdata {
+
+    private String wiki;
+    private String img;
+}
