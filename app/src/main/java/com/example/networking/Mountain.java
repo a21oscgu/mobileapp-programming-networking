da9d5ea02ad8598e84825b282ac0e410c4676980
a21oscgu <a21oscgu@student.his.se>
2022-04-29 09:39:31 +0200

    Added getters to the local JSON-data

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -35,4 +35,39 @@ public class Mountain {
     private int feet;
     private Auxdata auxdata;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getMeters() {
+        return meters;
+    }
+
+    public int getFeet() {
+        return feet;
+    }
+
+    public Auxdata getAuxdata() {
+        return auxdata;
+    }
 }
