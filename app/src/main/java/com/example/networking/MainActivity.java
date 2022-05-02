4b03bf19ad9655c3eb185ee49d7cff5cda9908a0
a21oscgu <a21oscgu@student.his.se>
2022-05-02 12:32:35 +0200

    The app is able to output names of mountains in the RecyclerView.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -9,10 +9,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
+    //private final String JSON_FILE = "mountains.json";
+
+    ArrayList<String> MountainsArray;
+
     RecyclerView recyclerView;
+
     private Mountain[] Mountains;
 
     @Override
@@ -20,20 +28,14 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.my_recyclerview);
-        recyclerView.setAdapter(new MyAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         //Hämta lokal JSON-data
         //new JsonFile(this, this).execute(JSON_FILE);
+
         //Starta nedladdning av JSON-data
         new JsonTask(this).execute(JSON_URL);
 
         //recyclerView.getAdapter().notifyDataSetChanged();
     }
-
-    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
-    //private final String JSON_FILE = "mountains.json";
 
     @Override
     public void onPostExecute(String json) {
@@ -41,8 +43,22 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
         Gson gson = new Gson();
         Mountains = gson.fromJson(json,Mountain[].class);
-        for (int i = 0; i < Mountains.length; i++){
-            Log.d("MainActivity==>","Mountain: "+Mountains[i].getName());
+
+        MountainsArray = new ArrayList<>();
+
+        for (Mountain mountain : Mountains) { // This line = "for (int i = 0; i < Mountains.length; i++){"
+
+
+            String name = mountain.getName();
+            MountainsArray.add(name);
+            MountainsArray.add("\n");
+
+            //Log.d("==>","Namn: " + name);
+            //Log.d("MainActivity==>","Mountain: "+Mountains[i].getName());
+
         }
+        recyclerView = findViewById(R.id.my_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(MountainsArray));
     }
 }
